e72da764ed7e88d0e4ab61e8d9071d465a951a4b
yunzhiy <yanyz@zju.edu.cn>
2025-03-09 07:51:25 +0000

    import fusedadam from runner to utils

--- easyvolcap/utils/adam_utils.py
+++ easyvolcap/utils/adam_utils.py
@@ -2,7 +2,108 @@ import math
 import torch
 from typing import List, Union, Optional
 from torch.utils.cpp_extension import load
+from torch.optim import Adam
 from easyvolcap.utils.console_utils import *
+
+class MyFusedAdam(Adam):
+    def step(self, closure=None):
+        """Perform a single optimization step.
+
+        Will disrespect weight decay, but significantly reduce kernel launches
+
+        Args:
+            closure (Callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        self._cuda_graph_capture_health_check()
+
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for i, group in enumerate(self.param_groups):
+            params_with_grad = []
+            grads = []
+            exp_avgs = []
+            exp_avg_sqs = []
+            max_exp_avg_sqs = []
+            state_steps = []
+            beta1, beta2 = group['betas']
+
+            has_complex = self._init_group(
+                group,
+                params_with_grad,
+                grads,
+                exp_avgs,
+                exp_avg_sqs,
+                max_exp_avg_sqs,
+                state_steps)
+
+            from easyvolcap.utils.adam_utils import _single_tensor_adam
+            _single_tensor_adam(
+                params_with_grad,
+                grads,
+                exp_avgs,
+                exp_avg_sqs,
+                max_exp_avg_sqs,
+                state_steps,
+                amsgrad=getattr(self, "amsgrad", False),
+                has_complex=has_complex,
+                beta1=beta1,
+                beta2=beta2,
+                lr=group['lr'],
+                weight_decay=group['weight_decay'],
+                eps=group['eps'],
+                maximize=getattr(self, "maximize", False),
+                capturable=getattr(self, "capturable", False),
+                differentiable=getattr(self, "differentiable", False),
+                grad_scale=getattr(self, "grad_scale", None),
+                found_inf=getattr(self, "found_inf", None),
+            )
+
+        return loss
+
+    def _init_group(
+        self,
+        group,
+        params_with_grad,
+        grads,
+        exp_avgs,
+        exp_avg_sqs,
+        max_exp_avg_sqs,
+        state_steps
+    ):
+        # Older version of PyTorch doesn't have this method
+        has_complex = False
+        for p in group['params']:
+            if p.grad is not None:
+                has_complex |= torch.is_complex(p)
+                params_with_grad.append(p)
+                if p.grad.is_sparse:
+                    raise RuntimeError('Adam does not support sparse gradients, please consider SparseAdam instead')
+                grads.append(p.grad)
+
+                state = self.state[p]
+                # Lazy state initialization
+                if len(state) == 0:
+                    # note(crcrpar): [special device hosting for step]
+                    # Deliberately host `step` on CPU if both capturable and fused are off.
+                    # This is because kernel launches are costly on CUDA and XLA.
+                    state['step'] = (
+                        torch.tensor(0.0, dtype=torch.float32)
+                    )
+                    # Exponential moving average of gradient values
+                    state['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                    # Exponential moving average of squared gradient values
+                    state['exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+
+                exp_avgs.append(state['exp_avg'])
+                exp_avg_sqs.append(state['exp_avg_sq'])
+
+                state_steps.append(state['step'])
+        return has_complex
+
 
 
 @torch.jit.script
